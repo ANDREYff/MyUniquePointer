43dae89299ea945bf82e20622d250b4a8555fb79
andrew <andrew_ov@protonmail.com>
2023-09-20 19:24:32 +0300

    removed logging

--- uniquePtr.h
+++ uniquePtr.h
@@ -1,17 +1,11 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
 
-#define LOG
-//#undef LOG
-#ifdef LOG
-#include <iostream>
-#endif
+#include <utility>
+
 template<typename T>
 struct Deleter {
 	void operator()(T* ptr) {
-#ifdef LOG
-		std::cout << "default deleter called" << std::endl;
-#endif
 		delete ptr;
 	}
 };
@@ -19,31 +13,19 @@ template<typename T, typename DD = Deleter<T>>
 class UP{
 	public:
 	UP(T* ptr = nullptr) : ptr_(ptr) {
-#ifdef LOG
-		std::cout << "UP c-tor" << std::endl;
-#endif
 	}
 	UP(const UP&) = delete;
 	UP& operator=(const UP&&) = delete;
 	UP(UP&& that) : ptr_(that.ptr_) {
-#ifdef LOG
-		std::cout << "UP move c-tor" << std::endl;
-#endif
 		that.ptr_ = nullptr;
 	}
 	UP& operator=(UP&& that) {
-#ifdef LOG
-		std::cout << "UP move assign" << std::endl;
-#endif
 		if(that == *this) { return this; }
 		defaultDeleter(ptr_);
 		ptr_ = that.ptr_;
 		that.ptr_ = nullptr;
 	}
 	~UP() {
-#ifdef LOG
-		std::cout << "UP d-tor" << std::endl;
-#endif
 		defaultDeleter(ptr_);
 	}
 	T& operator*() {
